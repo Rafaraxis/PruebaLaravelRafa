30a3ff5e5015029926ea06aea6579684eba47bb6
Alumno <ulsa@iMacD107.local>
2019-01-16 18:42:36 -0700

    first commit

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Contacto;
 
 class InicioController extends Controller
 {
     public function hola(){
-        return view('hola');
+        //Obtengo todos los registros de la tabla contactos
+        $contactos = Contacto::all();
+
+        //Argumentos con la info que va a autilizar la vista
+        $arumentos = array();
+        $argumentos['contactos'] = $contactos;
+
+        return view('hola', $argumentos);
     }
 }
--- resources/views/hola.blade.php
+++ resources/views/hola.blade.php
@@ -10,6 +10,24 @@
 </head>
 <body>
     <h1>Hola!</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Telefono</th>
+                <th>Correo</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach($contactos as $contacto)
+            <tr>
+                <th>{{ $contacto->nombre }}</th>
+                <th><{{ $contacto->telefono }}</th>
+                <th>{{ $contacto->correo }}</th>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
     <p>Esto es una pagina html</p>
 </body>
 </html>
